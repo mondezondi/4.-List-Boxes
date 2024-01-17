fc9c270badd5875083c659389e0e8eeaa642a329
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-17 10:27:28 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add("Redmi")
+
+        Dim myitem
+
+        myitem = InputBox("Samsung")
+        ListBox1.Items.Add(myitem)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myitem2
+        myitem2 = InputBox("Blackberry")
+        ListBox1.Items.Remove(myitem2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
